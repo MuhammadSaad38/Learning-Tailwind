7d34041831ebb0df071116a45a8d4e69ef443c25
Muhammad Saad <149876788+MuhammadSaad38@users.noreply.github.com>
2024-09-14 12:32:58 +0500

    Tailwind Installation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Protest: ["Protest Guerrilla", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
